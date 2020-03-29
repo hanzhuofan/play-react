f23015a2d58433ee63dfc12dd03cfe8cb28b8d74
hanzhuofan <310756323@qq.com>
2020-03-29 18:12:35 +0800

    redux 修复

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { Provider } from "react-redux";
+import store from "../store/store";
+import TodoApp from "../components/TodoApp";
 
 export default class Detail extends React.Component {
     render() {
         return (
             <div>
                 <a href='#/'>回到home</a>
+                <Provider store={store}>
+                    <TodoApp />
+                </Provider>
             </div>
         )
     }
