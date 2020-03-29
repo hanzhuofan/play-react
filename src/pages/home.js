97bb854bfed752fd71361f23494c8768540dd7f3
hanzhuofan <310756323@qq.com>
2020-03-29 13:10:09 +0800

    play

---
+++ src/pages/home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Table from "./Table";
+import Form from "./Form";
+
+
+export default class Home extends React.Component {
+    state = {
+        characters: [],
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index
+            }),
+        })
+    };
+
+    handleSubmit = character => {
+        this.setState({ characters: [...this.state.characters, character] });
+    };
+
+    render() {
+        const { characters } = this.state;
+
+        return (
+            <div>
+                <a href='#/detail'>去detail</a>
+                <div className="container">
+                    <Table characterData={characters} removeCharacter={this.removeCharacter} />
+                    <h3>Add New</h3>
+                    <Form handleSubmit={this.handleSubmit} />
+                </div>
+            </div>
+
+        )
+    }
+}
